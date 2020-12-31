df991643d3885258731161080a6e3c1e7b9517cf
ehalsey <ehalsey@a-cto.com>
2020-12-30 19:27:55 -0800

    working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Button from 'react-bootstrap/Button';
+import Main from './main/main'
 
 function App() {
     return (
@@ -10,6 +11,7 @@ function App() {
             <p>Edit <code>src/App.js</code> and save to reload.</p>
             <Button>Does this work Yes it does</Button>
             <a className = "App-link" href = "https://reactjs.org" target = "_blank" rel = "noopener noreferrer">Learn React</a>
+            <Main version="1"></Main>
         </header>
     </div>
     );
---
+++ src/main/main.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './main.css';
+const Main = vars => {
+  return (<div className="Main">App Version number #{vars.version}</div>);
+};
+export default Main;
