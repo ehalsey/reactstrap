3e42ce6b9a3973daed986ec2d8c29e7d49c44a39
ehalsey <ehalsey@a-cto.com>
2020-12-30 20:14:47 -0800

    working

--- src/main/main.js
+++ src/main/main.js
@@ -1,6 +1,34 @@
 import React from 'react';
+import Accordion from 'react-bootstrap/Accordion'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
 import './main.css';
 const Main = vars => {
-  return (<div className="Main">App Version number #{vars.version}</div>);
+  return (
+<div className="Main">      
+    <Accordion defaultActiveKey="0">
+    <Card>
+        <Card.Header>
+        <Accordion.Toggle as={Button} variant="link" eventKey="0">
+            Click me!
+        </Accordion.Toggle>
+        </Card.Header>
+        <Accordion.Collapse eventKey="0">
+        <Card.Body>Hello! I'm the body</Card.Body>
+        </Accordion.Collapse>
+    </Card>
+    <Card>
+        <Card.Header>
+        <Accordion.Toggle as={Button} variant="link" eventKey="1">
+            Click me!
+        </Accordion.Toggle>
+        </Card.Header>
+        <Accordion.Collapse eventKey="1">
+        <Card.Body>Hello! I'm another body</Card.Body>
+        </Accordion.Collapse>
+    </Card>
+    </Accordion>
+</div>
+  );
 };
 export default Main;
